6dee729f0a902a11c98b6e0ffd73ba4769591fa1
Deepu Mohan Puthrote <git@deepumohan.com>
2024-05-06 22:59:32 +0100

    Json editor is ready!

--- src/app.rs
+++ src/app.rs
@@ -1,3 +1,5 @@
+use std::collections::HashMap;
+
 pub enum CurrentScreen {
     Main,    // main summary screen
     Editing, // input key value pairs
@@ -12,16 +14,16 @@ pub enum CurrentlyEditing {
 
 // Application State
 pub struct App {
-    current_screen: CurrentScreen,
-    currently_editing: Option<CurrentlyEditing>,
-    key: String,
-    value: String,
-    pairs: HashMap<String, String>,
+    pub current_screen: CurrentScreen,
+    pub currently_editing: Option<CurrentlyEditing>,
+    pub key: String,
+    pub value: String,
+    pub pairs: HashMap<String, String>,
 }
 
-impl Default for App {
-    fn default() -> Self {
-        Self {
+impl App {
+    pub fn new() -> App {
+        App {
             current_screen: CurrentScreen::Main,
             currently_editing: None,
             key: String::new(),
@@ -30,14 +32,14 @@ impl Default for App {
         }
     }
 
-    fn save_key_value(&mut self) {
+    pub fn save_key_value(&mut self) {
         self.pairs.insert(self.key.clone(), self.value.clone());
         self.key.clear();
         self.value.clear();
         self.currently_editing = None;
     }
 
-    fn toggle_editing(&mut self) {
+    pub fn toggle_editing(&mut self) {
         if let Some(edit_mode) = &self.currently_editing {
             match edit_mode {
                 CurrentlyEditing::Key => {
@@ -52,7 +54,7 @@ impl Default for App {
         }
     }
 
-    fn print_json(&self) -> serde_json::Result<()> {
+    pub fn print_json(&self) -> serde_json::Result<()> {
         let json = serde_json::to_string_pretty(&self.pairs)?;
         println!("{}", json);
         Ok(())
